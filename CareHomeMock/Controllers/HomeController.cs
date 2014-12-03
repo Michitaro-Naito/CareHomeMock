352d6c0ef6e393b73cdc5bc4c63ad73ccb08e469
Michitaro Naito <m-naito@amlitek.com>
2014-12-03 16:15:03 +0900

    Add: CareHome Search #15

--- CareHomeMock/Controllers/HomeController.cs
+++ CareHomeMock/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CareHomeMock.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,121 @@ namespace CareHomeMock.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        /// <summary>
+        /// Map Search
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult CareHomeSearch()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult CareManagerSearch()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        public ActionResult CareHomeInfo_BasicInfo()
+        {
+            return View();
+        }
+
+        public ActionResult CareHomeInfo_CareManagers()
+        {
+            return View();
+        }
+
+        public ActionResult CareHomeInfo_Media()
+        {
+            return View();
+        }
+
+        public ActionResult CareHomeInfo_AccessMap()
+        {
+            return View();
+        }
+
+        public ActionResult CareManagerInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetSelectOptions()
+        {
+            var prefectures = db.Areas
+                .GroupBy(a => a.PrefectureCode)
+                .Select(a => new { id = a.FirstOrDefault().PrefectureCode, name = a.FirstOrDefault().PrefectureName })
+                .OrderBy(p => p.id)
+                .ToList();
+
+            var cities = db.Areas
+                .Select(a => new { id = a.CityCode, name = a.CityName })
+                .OrderBy(c => c.id)
+                .ToList();
+
+            var companyTypes = Enum.GetNames(typeof(CompanyType))
+                .Select(n => new { id = Enum.Parse(typeof(CompanyType), n), name = n })
+                .ToList();
+
+            return Json(new { prefectures = prefectures, cities = cities, companyTypes = companyTypes });
+        }
+
+        [HttpPost]
+        public ActionResult GetCareHomes(int? prefectureCode, int? cityCode, CompanyType? companyType, string keyword, int? page)
+        {
+            // Active
+            var q = db.CareHomes.Where(h => !h.Deactivated);
+
+            // Prefecture
+            if (prefectureCode != null)
+                q = q.Where(h => h.Area.PrefectureCode == prefectureCode.Value);
+
+            // City
+            if (cityCode != null)
+                q = q.Where(h => h.Area.CityCode == cityCode.Value);
+
+            // CompanyType
+            if (companyType != null)
+                q = q.Where(h => h.CompanyType == companyType.Value);
+
+            // Keyword
+            if (keyword != null)
+                q = q.Where(h => h.CompanyName.Contains(keyword) || h.Messages.Contains(keyword));
+
+            // Paging
+            var offset = 0;
+            if (page != null)
+                offset = 50 * page.Value;
+            var careHomes = q.OrderBy(h => h.CareHomeId).Skip(offset).Take(50).ToList()
+                .Select(h => new
+                {
+                    CareHomeId = h.CareHomeId,
+                    CompanyName = h.CompanyName,
+                    Address = h.Address,
+                    Years = 123,
+                    CareManagerCount = h.CareManagers.Count,
+                    ReviewCount = 456,
+                    Rating = 4.5
+                }).ToList();
+
+            return Json(careHomes);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ CareHomeMock/Controllers/MediaFileController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CareHomeMock.Models;
+
+namespace CareHomeMock.Controllers
+{
+    public class MediaFileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /MediaFile/
+        public ActionResult Index()
+        {
+            var mediafiles = db.MediaFiles.Include(m => m.CareHome);
+            return View(mediafiles.ToList());
+        }
+
+        // GET: /MediaFile/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MediaFile mediafile = db.MediaFiles.Find(id);
+            if (mediafile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mediafile);
+        }
+
+        // GET: /MediaFile/Create
+        public ActionResult Create()
+        {
+            ViewBag.CareHomeId = new SelectList(db.CareHomes, "CareHomeId", "CareHomeCode");
+            return View();
+        }
+
+        // POST: /MediaFile/Create
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="MediaFileId,Created,Updated,CareHomeId,Order,Type,RowKey")] MediaFile mediafile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MediaFiles.Add(mediafile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CareHomeId = new SelectList(db.CareHomes, "CareHomeId", "CareHomeCode", mediafile.CareHomeId);
+            return View(mediafile);
+        }
+
+        // GET: /MediaFile/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MediaFile mediafile = db.MediaFiles.Find(id);
+            if (mediafile == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CareHomeId = new SelectList(db.CareHomes, "CareHomeId", "CareHomeCode", mediafile.CareHomeId);
+            return View(mediafile);
+        }
+
+        // POST: /MediaFile/Edit/5
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="MediaFileId,Created,Updated,CareHomeId,Order,Type,RowKey")] MediaFile mediafile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mediafile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CareHomeId = new SelectList(db.CareHomes, "CareHomeId", "CareHomeCode", mediafile.CareHomeId);
+            return View(mediafile);
+        }
+
+        // GET: /MediaFile/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MediaFile mediafile = db.MediaFiles.Find(id);
+            if (mediafile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mediafile);
+        }
+
+        // POST: /MediaFile/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MediaFile mediafile = db.MediaFiles.Find(id);
+            db.MediaFiles.Remove(mediafile);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
