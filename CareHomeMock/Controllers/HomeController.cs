ee2d0384b89aa69318fec6e4005bda4b6c8c9c24
Michitaro Naito <m-naito@amlitek.com>
2014-12-04 15:09:57 +0900

    #16 CareManager Search

--- CareHomeMock/Controllers/HomeController.cs
+++ CareHomeMock/Controllers/HomeController.cs
@@ -103,6 +103,37 @@ namespace CareHomeMock.Controllers
         }
 
         [HttpPost]
+        public ActionResult GetSelectOptions_CareManagerSearch()
+        {
+            var prefectures = db.Areas
+                .GroupBy(a => a.PrefectureCode)
+                .Select(a => new { id = a.FirstOrDefault().PrefectureCode, name = a.FirstOrDefault().PrefectureName })
+                .OrderBy(p => p.id)
+                .ToList();
+
+            var cities = db.Areas
+                .Select(a => new { id = a.CityCode, name = a.CityName })
+                .OrderBy(c => c.id)
+                .ToList();
+
+            var genders = Enum.GetNames(typeof(Gender))
+                .Select(n => new { id = Enum.Parse(typeof(Gender), n), name = n })
+                .ToList();
+
+            var ageRanges = Enum.GetNames(typeof(AgeRange))
+                .Select(n => new { id = Enum.Parse(typeof(AgeRange), n), name = n })
+                .ToList();
+
+            return Json(new
+            {
+                prefectures = prefectures,
+                cities = cities,
+                genders = genders,
+                ageRanges = ageRanges
+            });
+        }
+
+        [HttpPost]
         public ActionResult GetCareHomes(int? prefectureCode, int? cityCode, CompanyType? companyType, string keyword, int? page)
         {
             // Active
@@ -141,6 +172,47 @@ namespace CareHomeMock.Controllers
                 }).ToList();
 
             return Json(careHomes);
+        }
+
+        public ActionResult GetCareManagers(int? prefectureCode, int? cityCode, Gender? gender, AgeRange? ageRange, bool? allowNewPatient /* licenses */)
+        {
+            // Active
+            var q = db.CareHomes.Where(h => !h.Deactivated);
+
+            // Prefecture
+            if (prefectureCode != null)
+                q = q.Where(h => h.Area.PrefectureCode == prefectureCode.Value);
+
+            // City
+            if (cityCode != null)
+                q = q.Where(h => h.Area.CityCode == cityCode.Value);
+
+            // CareManagers
+            var mq = q.SelectMany(h => h.CareManagers);
+
+            // Gender
+            if(gender != null)
+                mq = mq.Where(m => m.Gender == gender.Value);
+
+            // AgeRange
+            //if(ageRange != null)
+            //    careManagers = careManagers.Where(m => m.Birthday)
+
+            // AllowNewPatient
+            if (allowNewPatient != null)
+                mq = mq.Where(m => m.AllowNewPatient == allowNewPatient.Value);
+
+            // Licenses
+
+            var careManagers = mq.Select(m => new {
+                CareManagerId = m.CareManagerId,
+                Name = m.Name
+            }).ToList();
+
+            return Json(new 
+            {
+                careManagers = careManagers
+            });
         }
 
         protected override void Dispose(bool disposing)
---
+++ CareHomeMock/Models/Enum/AgeRange.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CareHomeMock.Models
+{
+    public enum AgeRange
+    {
+        All = 0,
+
+        UnderTen = 1,
+        Teens = 2,
+        Twenties = 3,
+        Thirties = 4,
+        Forties = 5,
+        Fifties = 6,
+        Sixties = 7,
+        Seventies = 8,
+        MoreThanEighty = 9
+    }
+}
