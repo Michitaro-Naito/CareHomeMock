aefe639b2b969404d3e84e9a0a00e2b48bb3fd27
Michitaro Naito <m-naito@amlitek.com>
2014-12-04 16:13:50 +0900

    #21 Website Design Basements

--- CareHomeMock/App_Start/BundleConfig.cs
+++ CareHomeMock/App_Start/BundleConfig.cs
@@ -24,8 +24,9 @@ namespace CareHomeMock
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/style.css",
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/font-awesome.css"));
         }
     }
 }
