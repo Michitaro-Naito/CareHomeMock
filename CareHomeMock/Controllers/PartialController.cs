11a140e057b6ab534c6c5c0037826452852d5ee4
Michitaro Naito <m-naito@amlitek.com>
2014-12-13 03:45:00 +0900

    #49 List StaticPages

---
+++ CareHomeMock/Controllers/PartialController.cs
@@ -0,0 +1,25 @@
+﻿using CareHomeMock.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CareHomeMock.Controllers
+{
+    public class PartialController : Controller
+    {
+        public ActionResult MainLinks()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var pages = db.StaticPage
+                    .OrderBy(p => p.Order)
+                    .ThenByDescending(p => p.Created)
+                    .ToList();
+
+                return View(pages);
+            }
+        }
+	}
+}
